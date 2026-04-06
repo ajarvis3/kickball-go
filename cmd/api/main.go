8f0a0b36b4c67aa3024b82b9d3c2fc097de1c877
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2026-04-06 18:06:02 -0400

    Add the main from copilot

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,23 +4,65 @@ import (
 	"context"
 	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/ajarvis3/kickball-go/code/internal/handlers"
 )
 
+var (
+	atBatHandler   = handlers.NewAtBatHandler()
+	gameHandler    = handlers.NewGameHandler()
+	teamHandler    = handlers.NewTeamHandler()
+	leagueHandler  = handlers.NewLeagueHandler()
+	playerHandler  = handlers.NewPlayerHandler()
+)
+
+func router(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Received request: %s %s", req.HTTPMethod, req.Path)
+
+	switch req.Path {
+	// ----- AtBats -----
+	case "/atbats":
+		if req.HTTPMethod == "POST" {
+			return atBatHandler.CreateAtBat(ctx, req)
+		}
+
+	// ----- Games -----
+	case "/games":
+		if req.HTTPMethod == "POST" {
+			return gameHandler.CreateGame(ctx, req)
+		}
+
+	// ----- Teams -----
+	case "/teams":
+		if req.HTTPMethod == "POST" {
+			return teamHandler.CreateTeam(ctx, req)
+		}
+
+	// ----- Leagues -----
+	case "/leagues":
+		if req.HTTPMethod == "POST" {
+			return leagueHandler.CreateLeague(ctx, req)
+		}
+
+	// ----- Players -----
+	case "/players":
+		if req.HTTPMethod == "POST" {
+			return playerHandler.CreatePlayer(ctx, req)
+		}
+	}
+
+	// Default 404
+	return events.APIGatewayProxyResponse{
+		StatusCode: 404,
+		Body:       `{"error":"route not found"}`,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}, nil
+}
+
 func main() {
-	lambda.Start(handler)
-}
-
-type Request struct {
-	// TODO: define request shape
-}
-
-type Response struct {
-	// TODO: define response shape
-}
-
-func handler(ctx context.Context, req Request) (Response, error) {
-	log.Println("kickball api invoked")
-	// TODO: route to handlers
-	return Response{}, nil
+	lambda.Start(router)
 }
